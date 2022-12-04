526afacfdbcd593ccbc9b2bee21db6bb91c3544b
GeorgeCrisan <georgerdp@gmail.com>
2022-12-04 17:13:37 +0000

    [chore] Add base functionality

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+module.exports = {
+   mode: "production",
+   entry: {
+      "ncce-background": path.resolve(__dirname, "src", "ncce-background.ts"),
+      ncce: path.resolve(__dirname, "src", "ncce.ts"),
+   },
+   output: {
+      path: path.join(__dirname, "/dist"),
+      filename: "[name].js",
+   },
+   resolve: {
+      extensions: [".ts", ".js"],
+   },
+   module: {
+      rules: [
+         {
+            test: /\.tsx?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/,
+         },
+      ],
+   },
+   plugins: [
+      new CopyPlugin({
+         patterns: [{from: ".", to: ".", context: "public"}]
+      }),
+   ],
+};
