03264f6f3d02ca0df75a126f043abd1ef5d4ab4c
GeorgeCrisan <georgerdp@gmail.com>
2022-12-04 19:38:58 +0000

    [chore] take the text

--- src/worker/ncce-background.ts
+++ src/worker/ncce-background.ts
@@ -1,7 +1,32 @@
 
 const getTextContent = (info: any) => {
-  console.log("ab", info);
-  console.log(window.getSelection());
+  const body = document.getElementsByTagName("body");
+  const html = document.getElementsByTagName("html");
+  let anchorElement = body && body[0] ? body[0] : html[0];
+
+  if (anchorElement) {
+    // Get hold of the parent element, body or html and select all the text
+    const selection: any = window.getSelection();
+    selection.selectAllChildren(anchorElement);
+    
+    const textContent = selection.toString();
+    selection.removeAllRanges();
+
+    const a = document.createElement('a');
+
+    // Prepare the payload for upload
+    const payload = JSON.stringify({ value: textContent });
+    const blob = new Blob([payload], {type: "application/json"});
+    const url = URL.createObjectURL(blob);
+
+    // Prepare link for download
+    a.setAttribute('href', url);
+    a.setAttribute('download', info.title.replace(/[^a-zA-Z ]/g, "") + ".json" || "textcontent.json");
+    a.click();
+
+    // Do clean up 
+    URL.revokeObjectURL(url);
+  }
 };
 
 chrome.action.onClicked.addListener((tab) => {
